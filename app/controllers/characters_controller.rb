fca264b4b9e3433026bb56b82a96c03531d6c590
Mac <jordan.dale.cummings@gmail.com>
2018-01-02 14:21:47 -0700

    integrated new and create actions for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,6 +5,19 @@ class CharactersController < ApplicationController
   end
 
   def new
+    @characters = Character.new
+  end
+
+  def create
+    @characters = Character.new(params.require(:character).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @characters.save
+        format.html { redirect_to characters_path, notice: 'Your character was successfully submitted.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
     subtitle: "Short character description",
     body: "Sample character data with some attributes; race, age, gender, class, background, history, backstory, personality, etc.",
     main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
